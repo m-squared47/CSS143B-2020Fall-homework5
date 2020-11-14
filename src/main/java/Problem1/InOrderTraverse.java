cdb0731bbb45907fba59e4c1a9d1d8a8e776dcfd
Martin Matsudaira <mmats23@uw.edu>
2020-11-14 11:56:51 -0800

    InOrderTraverse complete

--- src/main/java/Problem1/InOrderTraverse.java
+++ src/main/java/Problem1/InOrderTraverse.java
@@ -2,11 +2,34 @@ package Problem1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InOrderTraverse {
     public static List<Integer> inorderTraversalIterative(TreeNode<Integer> root) {
         // homework
         List<Integer> result = new ArrayList<>();
-        return result;  // place holder
+        Stack<TreeNode> nodes = new Stack<>();       //records previous nodes
+
+        if(root == null)                                //if tree is empty
+            return result;
+
+        if(root.left == null && root.right == null){    //if tree has one element
+            result.add(root.val);
+            return result;
+        }
+
+        while(root != null || nodes.size() > 0){        //go through tree
+
+            while(root != null){                        //transverse through leftmost branch
+                nodes.push(root);                       //push current node
+                root = root.left;                       //traverse left
+            }
+
+            root = nodes.pop();                         //get last non-null element
+            result.add(root.val);                       //push current value
+            root = root.right;                          //traverse right
+        }
+
+        return result;
     }
 }
--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -1,12 +1,53 @@
 package Problem1;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
+        Queue<Integer> values = new LinkedList<>();         //queue to store integers
+        Queue<TreeNode> nodes = new LinkedList<>();         //queue to store unused nodes
+
+        if(root == null){                                   //if empty
+            return result;
+        }
+
+        values.add(root.val);       //add root's value to queue
+        nodes.add(root);            //add root to queue
+
+        while(true){                    //loop until line break
+            int size = nodes.size();    //size of nodes queue
+            if(size == 0)               //break if nodes list is empty
+                break;
+
+            while(size != 0){
+                TreeNode current = nodes.peek();    //(temp) current node is head in nodes
+                                                    //the queue allows each level to be stored then the loop
+                                                    //can traverse through each level while removing used nodes
+                nodes.remove();                     //remove head
+
+                if(current.left != null){           //if left is not empty
+                    nodes.add(current.left);        //add left to nodes
+                }
+
+                if(current.right != null){          //if right is not empty
+                    nodes.add(current.right);       //add right to nodes
+                }
+
+                size--;                             //decrement size for this loop
+            }
+
+            for(TreeNode item : nodes){             //fill in values list
+                values.add(item.val);
+            }
+
+
+        }
+
         return result;  // place holder
     }
 }
