94d629c6a51e5e7297fb3f93fb16132c24c48618
Martin Matsudaira <mmats23@uw.edu>
2020-11-15 12:46:41 -0800

    lowestCommonAncestor complete

--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -9,26 +9,27 @@ public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
-        Queue<Integer> values = new LinkedList<>();         //queue to store integers
+        List<Integer> values = new LinkedList<>();          //queue to store integers
         Queue<TreeNode> nodes = new LinkedList<>();         //queue to store unused nodes
 
         if(root == null){                                   //if empty
-            return result;
+            return result;                                  //return null list
         }
 
-        values.add(root.val);       //add root's value to queue
-        nodes.add(root);            //add root to queue
+        values.add(root.val);           //add root's val to values list
+        nodes.add(root);                //add root to nodes queue
+        result.add(values);             //add foot's val to result
+        values.clear();                 //clear values list
 
         while(true){                    //loop until line break
             int size = nodes.size();    //size of nodes queue
             if(size == 0)               //break if nodes list is empty
                 break;
 
-            while(size != 0){
-                TreeNode current = nodes.peek();    //(temp) current node is head in nodes
+            for(int i = 0; i < size; i++){          //loop for every node in nodes queue
+                TreeNode current = nodes.poll();    //(temp) current node is head in nodes, remove head from nodes
                                                     //the queue allows each level to be stored then the loop
                                                     //can traverse through each level while removing used nodes
-                nodes.remove();                     //remove head
 
                 if(current.left != null){           //if left is not empty
                     nodes.add(current.left);        //add left to nodes
@@ -37,15 +38,15 @@ public class LevelOrderTraverse {
                 if(current.right != null){          //if right is not empty
                     nodes.add(current.right);       //add right to nodes
                 }
-
-                size--;                             //decrement size for this loop
             }
 
             for(TreeNode item : nodes){             //fill in values list
-                values.add(item.val);
+                values.add((int)item.val);          //the while loop should gather each level of
+                                                    //nodes into the nodes queue
             }
 
-
+            result.add(values);                             //put values list in result list
+            values.clear();                                 //clear values list
         }
 
         return result;  // place holder
--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -2,10 +2,36 @@ package Problem2;
 
 import Problem1.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
         // homework
         // hint: pay attention to the assumptions of this problem.
-        return null;    // place holder
+
+        //if root is null
+        //this should not be the case assuming p and q can be found in the root tree
+        if(root == null)
+            return null;
+
+        //if root equals an input, return this root
+        if(root.val == p.val || root.val == q.val)
+            return root;
+
+        //recursion to check each node in the tree
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        //if left and right have a node, return this root
+        if(left != null && right != null)
+            return root;
+
+        //if only right has a node, return root of right
+        if(right != null)
+            return right;
+
+        //else, return root of left
+        return left;
     }
 }
