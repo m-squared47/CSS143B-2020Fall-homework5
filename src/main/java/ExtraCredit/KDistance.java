ab63107ddd457db521eb5aea6ff137909c5c0f52
Martin Matsudaira <mmats23@uw.edu>
2020-11-15 17:56:48 -0800

    Problem 1 Finished

--- src/main/java/ExtraCredit/KDistance.java
+++ src/main/java/ExtraCredit/KDistance.java
@@ -1,6 +1,7 @@
 package ExtraCredit;
 
 import Problem1.TreeNode;
+import Problem1.InOrderTraverse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,58 @@ import java.util.List;
 public class KDistance {
     public static List<Integer> distanceK(TreeNode<Integer> root, TreeNode<Integer> target, int k) {
         // homework
+        //return all values that are "k" distance from the "target" of binary search tree "root"
+
+        //result arraylist
         List<Integer> result = new ArrayList<>();
+
+        //base case (root is null)
+        if(root == null)
+            return null;
+
+        //alt base case (target found)
+        if(root.val == target.val){
+            findValues(root, k, result);
+        }
+
+        //continue finding the target going to left and right sub trees
+        List<Integer> left = distanceK(root.left, target, k);
+        List<Integer> right = distanceK(root.right, target, k);
+
+        //if left has values
+        if(left != null){
+
+            for(int item : left){   //add values to result
+                result.add(item);
+            }
+        }
+
+        //if left has no values and right has values
+        if(right != null){
+
+            for(int item : right){  //add values to result
+                result.add(item);
+            }
+        }
+
         return result;  // place holder
+    }
+
+    /**
+     * Finds the values at distance k
+     * @param root
+     * @param distance
+     */
+    private static void findValues(TreeNode<Integer> root, int k, List<Integer> result){
+        if(root == null)            //base case (empty)
+            return;
+
+        if(k == 0){                  //alt base case (value found)
+            result.add(root.val);
+            return;
+        }
+
+        findValues(root.left, k - 1, result);   //if k is not 0, go left and right
+        findValues(root.right, k -1, result);
     }
 }
--- src/main/java/Problem1/LevelOrderTraverse.java
+++ src/main/java/Problem1/LevelOrderTraverse.java
@@ -9,46 +9,38 @@ public class LevelOrderTraverse {
     public static List<List<Integer>> levelOrder(TreeNode<Integer> root) {
         // homework
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> values = new LinkedList<>();          //queue to store integers
+        List<Integer> values = new ArrayList<>();          //list to store integers
         Queue<TreeNode> nodes = new LinkedList<>();         //queue to store unused nodes
+                                                            //I used linked list with the Queue interface
+                                                            //because it's easier for me to keep track of
+                                                            //and I can take out the oldest element in the list
+                                                            //this reason is important for later
 
-        if(root == null){                                   //if empty
-            return result;                                  //return null list
-        }
+        nodes.add(root);                                    //add root to nodes queue
+        while(!nodes.isEmpty() && root != null){            //while the nodes queue is not empty and a BST exists
+            int size = nodes.size();                        //size of nodes queue for this iteration
 
-        values.add(root.val);           //add root's val to values list
-        nodes.add(root);                //add root to nodes queue
-        result.add(values);             //add foot's val to result
-        values.clear();                 //clear values list
+            while(size > 0) {
+                TreeNode<Integer> hold = nodes.poll();      //set hold to first node, remove from nodes queue
+                values.add(hold.val);                       //add value to the values list
 
-        while(true){                    //loop until line break
-            int size = nodes.size();    //size of nodes queue
-            if(size == 0)               //break if nodes list is empty
-                break;
-
-            for(int i = 0; i < size; i++){          //loop for every node in nodes queue
-                TreeNode current = nodes.poll();    //(temp) current node is head in nodes, remove head from nodes
-                                                    //the queue allows each level to be stored then the loop
-                                                    //can traverse through each level while removing used nodes
-
-                if(current.left != null){           //if left is not empty
-                    nodes.add(current.left);        //add left to nodes
+                //check left, add branch to the queue if it exists
+                if (hold.left != null) {
+                    nodes.add(hold.left);
                 }
 
-                if(current.right != null){          //if right is not empty
-                    nodes.add(current.right);       //add right to nodes
+                //check right, add branch to the queue if it exists
+                if (hold.right != null) {
+                    nodes.add(hold.right);
                 }
+
+                size--;     //tells how long the loop should last
             }
 
-            for(TreeNode item : nodes){             //fill in values list
-                values.add((int)item.val);          //the while loop should gather each level of
-                                                    //nodes into the nodes queue
-            }
-
-            result.add(values);                             //put values list in result list
-            values.clear();                                 //clear values list
+            result.add(new ArrayList<>(values));    //add this level of values to the result
+            values.clear();                         //clear for next level of values
         }
 
-        return result;  // place holder
+        return result;  // return all levels of values
     }
 }
